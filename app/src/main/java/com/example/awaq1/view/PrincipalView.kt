38f6adb542dfad6fa969109c22a61b27accec61f
A01722540 <gabogal03@gmail.com>
2024-11-20 13:42:39 -0600

    Ooops
    my bad

--- app/src/main/java/com/example/awaq1/view/PrincipalView.kt
+++ app/src/main/java/com/example/awaq1/view/PrincipalView.kt
@@ -64,8 +64,8 @@ fun PrincipalView(modifier: Modifier = Modifier, auth0: Auth0) {
     val context = LocalContext.current as MainActivity
 
     Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-        context.accountInfo = AccountInfo("example@example.com", user_id = 1L)
-        if (true) { // loggedIn o true
+        //context.accountInfo = AccountInfo("example@example.com", user_id = 1L)
+        if (loggedIn) { // loggedIn o true
             AppNavigator(
                 onLogout = {
                     Log.d("AuthLogout", "Logging out!")
